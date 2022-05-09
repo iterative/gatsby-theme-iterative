00424f4f7f3b9a325bcf75dee73d3fa3f608c3fd
Thomas Kunwar <yathomasi@gmail.com>
2022-05-09 18:34:20 +0545

    added prism support for cml

--- packages/gatsby-theme-iterative/config/prismjs/dvc-hook.js
+++ packages/gatsby-theme-iterative/config/prismjs/dvc-hook.js
@@ -7,14 +7,12 @@ const argsRegex = new RegExp(/\-{1,2}[a-zA-Z-]*/, 'ig')
 // Make sure the $ part of the command prompt in shell
 // examples isn't copiable by making it an 'input' token.
 Prism.hooks.add('after-tokenize', env => {
-  if (env.language !== 'dvc') {
-    return
-  }
-
-  for (const token of env.tokens) {
-    if (token.type === 'line' && /^\$\s+$/.test(token.content[0])) {
-      const old = token.content[0]
-      token.content[0] = new Prism.Token('input', old, null, old, false)
+  if (['dvc', 'cml'].includes(env.language) && Array.isArray(env.tokens)) {
+    for (const token of env.tokens) {
+      if (token.type === 'line' && /^\$\s+$/.test(token.content[0])) {
+        const old = token.content[0]
+        token.content[0] = new Prism.Token('input', old, null, old, false)
+      }
     }
   }
 })
--- packages/gatsby-theme-iterative/config/prismjs/usage.js
+++ packages/gatsby-theme-iterative/config/prismjs/usage.js
@@ -1,11 +1,15 @@
 /* eslint-env node */
 
 const dvc = require('./dvc-commands')
+const cml = require('./cml-commands')
 const Prism = require('prismjs')
 
 Prism.languages.usage = {
   dvc: {
     pattern: new RegExp(`dvc (?:${dvc.join('|')})`)
+  },
+  cml: {
+    pattern: new RegExp(`cml (?:${cml.join('|')})`)
   },
   usage: {
     pattern: /(^|\n)\s*(usage|positional arguments|optional arguments)/
