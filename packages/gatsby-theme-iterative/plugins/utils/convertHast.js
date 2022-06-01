5dd2361ba769dc7557ad38b5b85eb775627315f4
Thomas Kunwar <yathomasi@gmail.com>
2022-06-01 15:03:09 +0545

    logging error

--- packages/gatsby-theme-iterative/plugins/utils/convertHast.js
+++ packages/gatsby-theme-iterative/plugins/utils/convertHast.js
@@ -1,14 +1,22 @@
 /** HAST - Hypertext Abstract Syntax Tree */
 async function convertHtmlToHast(htmlString) {
-  const { unified } = await import('unified')
-  const parse = (await import('rehype-parse')).default
-  return unified().use(parse, { fragment: true }).parse(htmlString)
+  try {
+    const { unified } = await import('unified')
+    const parse = (await import('rehype-parse')).default
+    return unified().use(parse, { fragment: true }).parse(htmlString)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 async function convertHastToHtml(htmlAst) {
-  const { unified } = await import('unified')
-  const stringify = (await import('rehype-stringify')).default
-  return unified().use(stringify).stringify(htmlAst)
+  try {
+    const { unified } = await import('unified')
+    const stringify = (await import('rehype-stringify')).default
+    return unified().use(stringify).stringify(htmlAst)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 module.exports = {
