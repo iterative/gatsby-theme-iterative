aa6ea76b137dc92993bbb90e514893c43d66034a
Thomas Kunwar <yathomasi@gmail.com>
2022-08-04 09:16:05 +0545

    component for opt out preredirect

--- packages/gatsby-theme-iterative/src/components/Documentation/Markdown/index.tsx
+++ packages/gatsby-theme-iterative/src/components/Documentation/Markdown/index.tsx
@@ -15,7 +15,7 @@ import rehypeReact from 'rehype-react'
 import Collapsible from 'react-collapsible'
 
 import Main from './Main'
-import Link from '../../Link'
+import Link, { NoPreRedirectLink } from '../../Link'
 import Tooltip from './Tooltip'
 import Admonition from './Admonition'
 
@@ -265,11 +265,7 @@ const renderAst = (slugger: Slugger) => {
     createElement: React.createElement,
     Fragment: React.Fragment,
     components: {
-      a: ({ children, ...props }: PropsWithChildren<{ href: string }>) => (
-        <Link {...props} optOutPreRedirect>
-          {children}
-        </Link>
-      ),
+      a: NoPreRedirectLink,
       abbr: Abbr,
       card: Card,
       cards: Cards,
