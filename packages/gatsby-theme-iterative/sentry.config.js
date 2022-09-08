3f4b1f2bcee821b12e58a5e626256ca451def7d9
rogermparent <rogermparent@gmail.com>
2022-09-08 15:52:55 -0400

    Add sentry-suggested decluttering from https://docs.sentry.io/platforms/javascript/guides/gatsby/configuration/filtering/#decluttering-sentry

--- packages/gatsby-theme-iterative/sentry.config.js
+++ packages/gatsby-theme-iterative/sentry.config.js
@@ -12,11 +12,50 @@ Sentry.init({
         fetch old chunks and will get ChunkLoadError, but then will load static
         page from the new deployed version and all will be ok. So we can just
         ignore these type of errors */
-    'ChunkLoadError'
+    'ChunkLoadError',
+    // from https://docs.sentry.io/platforms/javascript/guides/gatsby/configuration/filtering/#decluttering-sentry
+    // Random plugins/extensions
+    'top.GLOBALS',
+    // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
+    'originalCreateNotification',
+    'canvas.contentDocument',
+    'MyApp_RemoveAllHighlights',
+    'http://tt.epicplay.com',
+    "Can't find variable: ZiteReader",
+    'jigsaw is not defined',
+    'ComboSearch is not defined',
+    'http://loading.retry.widdit.com/',
+    'atomicFindClose',
+    // Facebook borked
+    'fb_xd_fragment',
+    // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
+    // reduce this. (thanks @acdha)
+    // See http://stackoverflow.com/questions/4113268
+    'bmi_SafeAddOnload',
+    'EBCallBackMessageReceived',
+    // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+    'conduitPage'
   ],
   /* There are some common urls which recomment to ignore. It's even 
       mentioned in the official documentation: https://docs.sentry.io/platforms/javascript/#decluttering-sentry
       In our case we just ignore all errors from the browser's extensions,
       because we can't influence on then somehow. */
-  blacklistUrls: [/extensions\//i, /^chrome:\/\//i]
+  blacklistUrls: [/extensions\//i, /^chrome:\/\//i],
+  // from https://docs.sentry.io/platforms/javascript/guides/gatsby/configuration/filtering/#decluttering-sentry
+  denyUrls: [
+    // Facebook flakiness
+    /graph\.facebook\.com/i,
+    // Facebook blocked
+    /connect\.facebook\.net\/en_US\/all\.js/i,
+    // Woopra flakiness
+    /eatdifferent\.com\.woopra-ns\.com/i,
+    /static\.woopra\.com\/js\/woopra\.js/i,
+    // Chrome extensions
+    /extensions\//i,
+    /^chrome:\/\//i,
+    // Other plugins
+    /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
+    /webappstoolbarba\.texthelp\.com\//i,
+    /metrics\.itunes\.apple\.com\.edgesuite\.net\//i
+  ]
 })
