78f99e38982176af6a44e6faaeee7cec3ff5ef2c
Thomas Kunwar <yathomasi@gmail.com>
2023-01-18 19:08:54 +0545

    throw error if image path is invalid (#167)

--- packages/gatsby-theme-iterative/plugins/image-preprocessor/index.js
+++ packages/gatsby-theme-iterative/plugins/image-preprocessor/index.js
@@ -1,20 +1,52 @@
 const path = require('path')
+const { find } = require('lodash')
 
 module.exports = async (
-  { markdownNode, markdownAST },
-  { baseDir = path.resolve('static') }
+  { files, getRemarkFileDependency, markdownNode, markdownAST },
+  { staticFolderName = 'static' }
 ) => {
   if (!markdownNode.fileAbsolutePath) return
   const { visit } = await import('unist-util-visit')
-
+  const baseDir = path.resolve(staticFolderName)
   const directory = path.dirname(markdownNode.fileAbsolutePath)
 
   // Process all markdown image nodes
-  visit(markdownAST, node => {
-    if (node.type === 'image') {
+  visit(markdownAST, async node => {
+    if (
+      node.type === 'image' &&
+      !(node.url?.startsWith('http://') || node.url?.startsWith('https://'))
+    ) {
       const { url } = node
-      const newUrl = path.relative(directory, path.join(baseDir, url))
-      node.url = newUrl
+      const imagePath = path.resolve(directory, path.join(baseDir, url))
+
+      let imageNode
+      if (getRemarkFileDependency) {
+        imageNode = await getRemarkFileDependency({
+          absolutePath: {
+            eq: imagePath
+          }
+        })
+      } else {
+        // Legacy: no context, slower version of image query
+        imageNode = find(files, file => {
+          if (file && file.absolutePath) {
+            return file.absolutePath === imagePath
+          }
+          return null
+        })
+      }
+
+      if (
+        (!imageNode || !imageNode.absolutePath) &&
+        process.env.NODE_ENV === `production`
+      ) {
+        throw new Error(
+          `Image Not Found: Image "${url}" not found in folder "${staticFolderName}" referenced from "${markdownNode.fileAbsolutePath}". Please check static folder name and that file exists at "${staticFolderName}${url}".`
+        )
+      } else {
+        const newUrl = path.relative(directory, path.join(baseDir, url))
+        node.url = newUrl
+      }
     }
   })
 }
