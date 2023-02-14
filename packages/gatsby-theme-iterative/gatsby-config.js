ea9ac8aeb4daaa2936c79e6eee81b12eba5d08d8
Roger <rogermparent@gmail.com>
2023-02-14 15:13:32 -0500

    Unify linting across websites repos (#173)
    * Copy new lint configs from dvc.org
    
    * Remove outdated workflow file and add concurrency to new one
    
    * Add new lint config and fix stylelint issues
    
    * Run fix-all
    
    * Comment out unused tests from workflow
    
    * Upgrade ESLint and Stylelint, remove deprecated stylelint-config-prettier
    
    * Run fix-all
    
    * Remove options from restyled config
    
    * Update commands and lint-staged
    
    * Add explicit prettier config argument to restyled
    
    * Run prettier on yml
    
    * Remove commented steps

--- packages/gatsby-theme-iterative/gatsby-config.js
+++ packages/gatsby-theme-iterative/gatsby-config.js
@@ -38,13 +38,6 @@ module.exports = ({
 }) => ({
   plugins: [
     {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true,
-        allExtensions: true
-      }
-    },
-    {
       resolve: 'gatsby-plugin-postcss',
       options: {
         postCssPlugins
